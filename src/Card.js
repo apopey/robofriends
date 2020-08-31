d3a7115b1ff723801ec0132a32f3537027de2600
apopey <andopopey@gmail.com>
2020-08-31 17:59:58 +0300

    End of 209. React App 1

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Card = ({ id, name, email }) => {
+    return (
+        <div className='tc bg-light-blue dib br3 pa2 ma2 grow bw2 shadow-5'>
+            <img alt='robots' src={`http://robohash.org/${id}?200x200`} />
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
+
+
